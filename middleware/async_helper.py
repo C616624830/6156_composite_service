c36b7efa27c63195e1204dc3d272605c99b933c4
Lieyang Chen <chen616624830@gmail.com>
2021-12-08 19:05:52 +0800

    Update async_helper.py

--- middleware/async_helper.py
+++ middleware/async_helper.py
@@ -12,12 +12,12 @@ async def main(cat_id, breeder_id, headers):
 
 async def search_cat(cat_id, headers):
     para = {"id": cat_id}
-    res = requests.get(url="https://d25a811kxhsede.cloudfront.net/dev/cats", params=para, headers=headers)
-    print("cat_res: ", res.json())
+    res = requests.get(url="https://zhm5v3fgp5.execute-api.us-east-2.amazonaws.com/dev/cats", params=para, headers=headers)
+    print("cat_res: ", res)
     return res
 
 async def search_breeder(breeder_id, headers):
     para = {"id": breeder_id}
-    res = requests.get(url="https://d25a811kxhsede.cloudfront.net/dev/breeders", params=para, headers=headers)
-    print("breeder_res: ", res.json())
+    res = requests.get(url="https://zhm5v3fgp5.execute-api.us-east-2.amazonaws.com/dev/breeders", params=para, headers=headers)
+    print("breeder_res: ", res)
     return res
